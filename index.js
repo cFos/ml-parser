6224ccca684f1da165ee072462a40425f756ca23
ArmorDarks <arkahor@gmail.com>
2016-10-24 00:58:44 +0300

    [js] by default filter out whitespace, newline and assign tokens

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { readFileSync } = require('fs')
+const { TOKEN_LANGASSIGN, TOKEN_WHITESPACE, TOKEN_NEWLINE } = require('./lexer')
 const { Parser } = require('./parser')
 
 let file = readFileSync('input.cfos.txt', 'utf8')
 
-new Parser(file)
+new Parser(file, [TOKEN_LANGASSIGN, TOKEN_WHITESPACE, TOKEN_NEWLINE])
