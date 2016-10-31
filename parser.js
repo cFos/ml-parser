95b9879bed1614431b71715d2518f5f0328d2c37
ArmorDarks <arkahor@gmail.com>
2016-11-01 00:41:30 +0200

    [js] improve parser

--- parser.js
+++ parser.js
@@ -1,4 +1,16 @@
-const { Lexer, TOKEN_WHITESPACE, TOKEN_NEWLINE } = require('./lexer')
+const {
+  Lexer,
+  TOKEN_PRAGMA,
+  TOKEN_PRAGMA_VALUE,
+  TOKEN_LANGKEY,
+  TOKEN_LANGKEY_ASSIGN,
+  TOKEN_LANGKEY_STRING,
+  TOKEN_LANGKEY_END,
+  TOKEN_COMMENT,
+  TOKEN_INLINE_COMMENT,
+  TOKEN_WHITESPACE,
+  TOKEN_NEWLINE
+} = require('./lexer')
 
 const isAnyWhitespace = (input) => input.type === TOKEN_WHITESPACE || input.type === TOKEN_NEWLINE
 
@@ -6,6 +18,7 @@ class Parser {
   constructor(str) {
     this.lexer = new Lexer(str)
     this.peeked = null
+    this.current = null
   }
 
   nextToken() {
@@ -23,13 +36,35 @@ class Parser {
       tok = this.lexer.nextToken()
     }
 
+    this.current = tok
     return tok
   }
 
   peekToken() {
     this.peeked = this.peeked || this.nextToken()
-
     return this.peeked
+  }
+
+  pushToken(tok) {
+    if (this.peeked) throw new Error('[pushToken] can only push one token on between reads')
+    this.peeked = tok
+  }
+
+  skip(type) {
+    const tok = this.nextToken()
+
+    if (!tok || tok.type !== type) {
+      this.pushToken(tok)
+      return false
+    }
+
+    return true
+  }
+
+  expect(type, onCurrent = false) {
+    const tok = onCurrent ? this.current : this.nextToken()
+    if (tok.type !== type) this.error(`expected ${type}, got ${tok.type}`, tok.lineno, tok.colno)
+    return tok
   }
 
   error(msg, lineno, colno) {
@@ -42,13 +77,50 @@ class Parser {
     throw new Error(`${msg}, ${lineno}:${colno}`)
   }
 
+  parseStatement() {
+    const { type, value } = this.current
+    let nextToken
+
+    switch (type) {
+
+      case TOKEN_PRAGMA:
+        this.peekToken()
+        nextToken = this.expect(TOKEN_PRAGMA_VALUE)
+        return { type, [value]: nextToken.value }
+
+      case TOKEN_COMMENT:
+      case TOKEN_INLINE_COMMENT:
+        return { type, value }
+
+      case TOKEN_LANGKEY:
+        this.expect(TOKEN_LANGKEY_ASSIGN)
+
+        let strings = []
+        nextToken = this.expect(TOKEN_LANGKEY_STRING)
+
+        while (nextToken.type === TOKEN_LANGKEY_STRING) {
+          strings.push(nextToken.value)
+          nextToken = this.nextToken()
+        }
+
+        this.expect(TOKEN_LANGKEY_END, true)
+
+        return { type, [value]: strings }
+
+      default:
+        this.error(`Unknown token ${type}`)
+    }
+  }
+
   parse() {
     let tok
     let buf = []
 
     while (tok = this.nextToken()) {
       const { value, lineno, colno } = tok
-      buf.push({ value, lineno, colno })
+      // console.log(tok)
+      const node = this.parseStatement()
+      buf.push(Object.assign(node, { lineno, colno }))
     }
 
     return buf
