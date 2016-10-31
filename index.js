5e27e460a4d4b73dcd60ada165228e10bfc52f49
ArmorDarks <arkahor@gmail.com>
2016-10-31 22:26:49 +0200

    [js] improve parser to pretend better for name of parser

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { readFileSync } = require('fs')
-const { TOKEN_LANGKEY_ASSIGN, TOKEN_WHITESPACE, TOKEN_NEWLINE } = require('./lexer')
 const { Parser } = require('./parser')
 
 let file = readFileSync('input.cfos.txt', 'utf8')
 
-new Parser(file, [TOKEN_LANGKEY_ASSIGN, TOKEN_WHITESPACE, TOKEN_NEWLINE])
+const parser = new Parser(file)
+console.log(parser.parse())
--- parser.js
+++ parser.js
@@ -1,19 +1,57 @@
-const { Lexer } = require('./lexer')
+const { Lexer, TOKEN_WHITESPACE, TOKEN_NEWLINE } = require('./lexer')
+
+const isAnyWhitespace = (input) => input.type === TOKEN_WHITESPACE || input.type === TOKEN_NEWLINE
 
 class Parser {
-  constructor(str, filter = []) {
+  constructor(str) {
     this.lexer = new Lexer(str)
-    this.AST = []
+    this.peeked = null
+  }
 
-    const { lexer, AST } = this
+  nextToken() {
+    let tok
 
-    while (!lexer.isFinished()) {
-      let token = lexer.nextToken()
-
-      if (token && !filter.includes(token.type)) AST.push(token)
+    if (this.peeked) {
+      tok = this.peeked
+      this.peeked = null
+      return tok
     }
 
-    console.log(AST)
+    tok = this.lexer.nextToken()
+
+    while(tok && isAnyWhitespace(tok)) {
+      tok = this.lexer.nextToken()
+    }
+
+    return tok
+  }
+
+  peekToken() {
+    this.peeked = this.peeked || this.nextToken()
+
+    return this.peeked
+  }
+
+  error(msg, lineno, colno) {
+    if ((lineno === undefined || colno === undefined) && this.peekToken()) {
+      const tok = this.peekToken()
+      lineno = tok.lineno
+      colno = tok.colno
+    }
+
+    throw new Error(`${msg}, ${lineno}:${colno}`)
+  }
+
+  parse() {
+    let tok
+    let buf = []
+
+    while (tok = this.nextToken()) {
+      const { value, lineno, colno } = tok
+      buf.push({ value, lineno, colno })
+    }
+
+    return buf
   }
 }
 
